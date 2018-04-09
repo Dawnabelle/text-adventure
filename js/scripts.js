f208561dae0a25cc44c40aa6bbba1e0f2efff815
Ami Cooper <amiraine.cooper@gmail.com>
2018-04-09 13:52:45 -0700

    something

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 //backend & globals
 var roomOneNarr =['','Its damp, earthy smell overtakes your senses.', 'You look around and notice two paths: one on your left and one on your right.', 'Which path will you choose?']
-
+function User() {
+  this.name = name;
+  this.hp = 100;
+  this.special = 0;
+}
 //front end
 $(document).ready(function(){
   $("#start").submit(function(event) {
+    event.preventDefault();
     var inputName = $("#name").val();
     var player = new User(inputName);
     var enemy = new Enemy();
-    event.preventDefault();
     $("#attack").click(function() {
       enemy.userAttack();
       player.enemyAttack();
@@ -17,19 +21,19 @@ $(document).ready(function(){
     });
     $("#special").click(function() {
       enemy.special();
+      console.log(enemy);
       $("#special").hide();
       player.special = 0;
     });
     $(".titleCard").slideUp();
     $(".adventureTime").slideDown();
-    var indexOne = 0;
-    $(".nextText").click(function(event){
-      event.preventDefault();
-      indexOne+=1
-      $(".textHere").text(roomOneNarr[indexOne]);
-      if(indexOne === 3){
-        $(".choiceOne").show();
-      }
-    })
   });
-});
+  var indexOne = 0;
+  $(".nextText").click(function(event){
+    event.preventDefault();
+    indexOne+=1
+    $(".textHere").text(roomOneNarr[indexOne]);
+    // if(indexOne === 3){
+    //   $()
+  })
+  })
