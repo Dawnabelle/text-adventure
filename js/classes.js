90055d317ecf987394cfa7eea456312841f6ca06
Bobby Martin <robert.martin.619@gmail.com>
2018-04-09 15:36:07 -0700

    Add all classes with appropriate values

---
+++ js/classes.js
@@ -0,0 +1,21 @@
+function Warrior(name) {
+  this.name = name;
+  this.charClass = "Warrior";
+  this.health = 100;
+  this.damage = 5;
+  this.special = 0;
+}
+function Wizard(name) {
+  this.name = name;
+  this.charClass = "Wizard";
+  this.health = 70;
+  this.damage = 8;
+  this.special = 0;
+}
+function Thief(name) {
+  this.name = name;
+  this.charClass = "Thief";
+  this.health = 80;
+  this.damage = 7;
+  this.special = 0;
+}
