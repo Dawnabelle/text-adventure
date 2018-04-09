a9bf44d65f81fbf89100ec0ac6875b112cd48c2d
Ami Cooper <amiraine.cooper@gmail.com>
2018-04-09 13:28:46 -0700

    fix branching issues

---
+++ js/rooms.js
@@ -0,0 +1,19 @@
+// BACK END
+function roomDialogue(){
+  this.words = [];
+};
+function attackDialogue() {
+  this.attack = [];
+};
+attackDialogue.prototype.hit = function () {
+  var hit = "You hit the skeleton for so much damage that it burst into pieces";
+  this.attack.push(hit);
+};
+function damageDialogue() {
+  this.damage = [];
+};
+damageDialogue.prototype.damage = function () {
+  var damage = "You took damage";
+  this.damage.push(damage);
+};
+//Text
---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+//backend & globals
+var roomOneNarr =['','Its damp, earthy smell overtakes your senses.', 'You look around and notice two paths: one on your left and one on your right.', 'Which path will you choose?']
+
+//front end
+$(document).ready(function(){
+  $("#start").submit(function(event) {
+    var inputName = $("#name").val();
+    var player = new User(inputName);
+    var enemy = new Enemy();
+    event.preventDefault();
+    $("#attack").click(function() {
+      enemy.userAttack();
+      player.enemyAttack();
+      if (player.special >= 30) {
+        $("#special").show();
+      }
+    });
+    $("#special").click(function() {
+      enemy.special();
+      console.log(enemy);
+      $("#special").hide();
+      player.special = 0;
+    });
+    $(".titleCard").slideUp();
+    $(".adventureTime").slideDown();
+  });
+  var indexOne = 0;
+  $(".nextText").click(function(event){
+    event.preventDefault();
+    indexOne+=1
+    $(".textHere").text(roomOneNarr[indexOne]);
+    // if(indexOne === 3){
+    //   $()
+    }
+  })
+
+});
