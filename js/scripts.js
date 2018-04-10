40d47be36ec05c61b2d0021e1c6f46d541ae30cb
Garnett Puett <gpuett@gmail.com>
2018-04-10 13:24:00 -0700

    end combat scenario.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //backend & globals
-var strikeText = ['You throw an elbow at your enemy and the hit lands heavy. You can tell you caused some damage.', 'You strike the enemy with your torch and it staggers back. It catches its footing and races towards you.', 'You lunge at your enemy with all of your might, lifting it off of its feet and slamming it into the ground.', 'Your left hook connects with the face of your foe. Causing physical and emotional damage.', 'You hammer pound your enemy on the top of the skull with audible thunk. It turns to run and you kick it in the back.', 'You hit the enemy and it stumbles. It regains its composure and quickly reaches for you.']
+var strikeText = ['You throw an elbow at your enemy and the hit lands heavy. You can tell you caused some damage.', 'You strike the enemy with your torch and it staggers back. It catches its footing and races towards you.', 'You lunge at your enemy with all of your might, lifting it off of its feet and slamming it into the ground.', 'Your left hook connects with the face of your foe. Causing physical and emotional damage.', 'You hammer pounds your enemy on the top of the skull with audible thunk. It turns to run and you kick it in the back.', 'You hit the enemy and it stumbles. It regains its composure and quickly reaches for you.']
 var missText = ['You slip as you move to strike and your hit doesn’t land. Try to hit it again?', 'Your punch lands short. You’ve done no damage.', 'Your kick doesn’t connect. The enemy has taken no damage.', 'Your enemy darts aside and you stumble past it clumsily.', 'You under reach and your attack doesn’t connect.']
 var dodgeText = ['With the grace of a ferret, you dart behind a narrow chasm and your enemy slams his fist in the wall.', 'You spin to your right and avoid your enemy’s attack.', 'You duck, narrowly missing a heavy throw from your enemy.', 'You jump back and watch as your attacker\'s punch just misses your gut.', 'Your enemy throws a punch and while you go to block, you slip. This send your enemy tumbling over you, giving you a chance to hold your ground.']
 var enemyStrikeText = ['With stunning force, the enemy lunges and hits you with all of its might.', 'The enemy bludgeons you with its spiky club.', 'Your enemy swings a giant club your way, connecting with your shoulder and sending waves of shock through your body.', 'You kick at the skeleton and miss. It throws you against the wall with it’s boney arms. ', 'The enemy claws you with its dirty, stinky talons.','The enemy’s forceful blow knocks the breath right out of your lungs. Gasping, you roll out of the way before it hits you again.', 'The enemy kicks you in the knee and laughs as you fall to the floor.'
@@ -7,7 +7,7 @@ var enemyStrikeText = ['With stunning force, the enemy lunges and hits you with 
 var roomStartNarr =['','Its damp, earthy smell overtakes your senses.', 'You look around and notice two paths: one on your left and one on your right.', 'Which path will you choose?']
 var roomOneNarr = ['', 'Lying on your belly, you begin a scramble into an equally small room', 'There isn’t anything in here. Just some mushrooms on the ground and some moist moss.']
 var roomTwoNarr = ['', 'As your eyes begin to adjust to the low light, you see a human skeleton on the floor.', 'Some how, some way, the skeleton begins to jostle.', ' It has noticed your appearance in its eternal chamber and has begun stumbling towards you.','Closer and closer it comes, readying its attack.', 'Prepare yourself for a fight!']
-var roomTwoAfterFight = ['','You hit the skeleton with so much force, it cascades boney shards in every direction.', 'You rest a minute and try to collect your feelings.', 'What just happened?', 'The scattered bones move no more.', 'Sensing no other immediate threat, you proceed.']
+var roomTwoAfterFight = ['', 'You rest a minute and try to collect your feelings.', 'What just happened?', 'The scattered bones move no more.', 'Sensing no other immediate threat, you proceed.']
 var roomThree = ['', 'Holding your breath, scared and nervous, you progress deeper into the cavern.’, ‘There are no enemies in this room, save the path itself.’, ‘A rickety bridge is in front of you and is the only way across.’, ‘You step out with your right foot and the bridge begins to sway.’, ‘Anxiously, you press on.']
 //front end
 $(document).ready(function(){
@@ -19,6 +19,7 @@ $(document).ready(function(){
     event.preventDefault();
     $("#attack").click(function() {
       $("#userHP").text(player.hp)
+      $(".combat-text").show();
       console.log(player);
       enemy.userAttack();
       if ((enemy.enemyRoll[enemy.enemyRoll.length-1]) === 20) {
@@ -34,8 +35,13 @@ $(document).ready(function(){
       } else {
         $(".enemy-text").text(enemyStrikeText[Math.floor(Math.random()*enemyStrikeText.length)]);
       }
-      $("#textTwo").hide();
-      $(".combat-text").show();
+      if (enemy.hp <= 0) {
+        $(".combat-text").text('You hit the skeleton with so much force, it cascades boney shards in every direction.');
+        $(".enemy-text").hide();
+        $("#attack").hide();
+        $(".afterFight").show();
+      }
+      // $("#textTwo").hide();
       // if (player.special >= 30) {
       //   $("#special").show();
       // }
@@ -103,6 +109,16 @@ $(document).ready(function(){
     $(".nextTwo").hide();
     $(".combat").show();
     $(".combat-text").hide();
+    }
+  });
+  var indexAfterTwoFight = 0;
+  $(".afterFight").click(function(event) {
+    $(".combat-text").hide();
+    indexAfterTwoFight +=1;
+    $("#afterFightText").text(roomTwoAfterFight[indexAfterTwoFight]);
+    if (indexAfterTwoFight === 3) {
+      $(".afterFight").hide();
+      $(".roomThree").show();
     }
   });
 });
